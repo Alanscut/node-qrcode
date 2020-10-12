87bd86e9d5f0a5638d25e35e8e5c93dcab4fd14a
Alanscut <wp_scut@163.com>
2020-10-12 17:33:27 +0800

    add svg stream support

--- examples/save.js
+++ examples/save.js
@@ -1,3 +1,4 @@
+const fs = require('fs')
 const QRCode = require('../lib')
 
 const path = './tmp.png'
@@ -5,7 +6,19 @@ QRCode.toFile(path, 'life of the party bros', {
   color: {
     dark: '#00F', // Blue modules
     light: '#0000' // Transparent background
-  }
+  },
+}, function (err) {
+  if (err) throw err
+  console.log('saved.')
+})
+
+const ws = fs.createWriteStream('./tmp.svg')
+QRCode.toFileStream(ws, 'life of the party bros', {
+  color: {
+    dark: '#00F', // Blue modules
+    light: '#0000' // Transparent background
+  },
+  type: 'svg'
 }, function (err) {
   if (err) throw err
   console.log('saved.')
